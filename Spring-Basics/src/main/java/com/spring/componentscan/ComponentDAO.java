57101f5edc10a5c1403678fe1813919484fc4c4e
Harshad Kawale <harshad.kawale@gmail.com>
2022-10-14 05:46:18 +0530

    Using Component Scan to scan for beans

---
+++ Spring-Basics/src/main/java/com/spring/componentscan/ComponentDAO.java
@@ -0,0 +1,25 @@
+package com.spring.componentscan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.spring.springin5steps.scope.JdbcConnection;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class ComponentDAO {
+
+	@Autowired
+	JdbcConnection jdbcconnection;
+
+	public JdbcConnection getJdbcconnection() {
+		return jdbcconnection;
+	}
+
+	public void setJdbcconnection(JdbcConnection jdbcconnection) {
+		this.jdbcconnection = jdbcconnection;
+	}
+		
+}
